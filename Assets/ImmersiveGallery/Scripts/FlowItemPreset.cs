c8ba21ff015af6710b9f6426808361c80922eb63
Hyunwoo Kim <hyunwookimbob@gmail.com>
2023-06-21 14:24:43 +0900

    #12 restructuring project and refactoring flow nav

---
+++ Assets/ImmersiveGallery/Scripts/FlowItemPreset.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Gallery
+{
+    [CreateAssetMenu(fileName = "ItemPreset", menuName = "ImmersiveGallery/Preset/FlowItemPreset")]
+    public class FlowItemPreset : ScriptableObject
+    {
+        #region Variable Field
+        public int Id = -1;
+        public string Name = null;
+        public AssetReference ItemRef = null;
+
+        #endregion
+    }
+}
