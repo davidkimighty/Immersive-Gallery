e49e24b4f4ecdecdda96ad22e13503e4fe2b2540
Hyunwoo Kim <hyunwookimbob@gmail.com>
2023-07-07 16:20:32 +0900

    #17 last color change

--- Assets/ImmersiveGallery/Scripts/Managers/TitleManager.cs
+++ Assets/ImmersiveGallery/Scripts/Managers/TitleManager.cs
@@ -10,10 +10,16 @@ namespace Gallery
         
         [SerializeField] private SceneAddressablePreset _immersiveGalleryScene = null;
 
+        [SerializeField] private Canvas _canvas = null;
+
         private ScreenFader _screenFader = null;
 
         private void Awake()
         {
+            if (_canvas.renderMode != RenderMode.ScreenSpaceCamera)
+                _canvas.renderMode = RenderMode.ScreenSpaceCamera;
+            _canvas.worldCamera = Camera.main;
+
             _screenFader = FindObjectOfType<ScreenFader>();
             _screenFader.FadeAsync(0);
         }
